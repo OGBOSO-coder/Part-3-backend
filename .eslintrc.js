7add5544dea4e7d394c5e67e8f414a0de2b03c80
OGBOSO-coder <Kim.tiainen@gmail.com>
2024-02-11 15:42:40 +0200

    kai tää toimiii

---
+++ .eslintrc.js
@@ -0,0 +1,75 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true,
+		"node": true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	},
+	'plugins': [
+        '@stylistic/js'
+    ],
+    'extends': 'eslint:recommended',
+    'rules': {
+        '@stylistic/js/indent': [
+            'error',
+            2
+        ],
+        '@stylistic/js/linebreak-style': [
+            'error',
+            'unix'
+        ],
+        '@stylistic/js/quotes': [
+            'error',
+            'single'
+        ],
+        '@stylistic/js/semi': [
+            'error',
+            'never'
+        ],
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		],
+		'no-console': 0,
+	  },
+	}
+
--- index.js
+++ index.js
@@ -1,107 +1,107 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('dist'))
 const mongoose = require('mongoose')
-const morgan = require(`morgan`)
-app.use(morgan(`tiny`))
+const morgan = require('morgan')
+app.use(morgan('tiny'))
 
 require('dotenv').config()
 const Person = require('./models/person')
 
-morgan.token(`body`, function (req, res) {
-  return `${JSON.stringify(req.body)}`
+morgan.token('body', function (req, res) {
+	return `${JSON.stringify(req.body)}`
 })
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 
-app.get("/api/persons",(req, res) => {
-  Person.find({}).then(Persons => {
-    res.json(Persons)
-  })
+app.get('/api/persons',(req, res) => {
+	Person.find({}).then(Persons => {
+		res.json(Persons)
+	})
 }) 
 
-app.get("/info",(req, res,) => {
-  const currentDate = new Date()
-  res.send(`<h2>Phonebook has info for ${Persons.length} people</h2> <h2>${currentDate}</h2>`)
+app.get('/info',(req, res,) => {
+	const currentDate = new Date()
+	res.send(`<h2>Phonebook has info for ${Persons.length} people</h2> <h2>${currentDate}</h2>`)
 }) 
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-      .then(person => {
-          if (person) {
-              response.json(person)
-          } else {
-              response.status(404).end()
-          }
-      })
-      .catch(error => next(error))
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 }) 
 
-app.delete("/api/persons/:id",(request, response) => {
-  Person
-  .findByIdAndDelete(request.params.id)
-  .then(result => {
-      response.json(result).status(204)
-  })
-  .catch(error => next(error))
+app.delete('/api/persons/:id',(request, response) => {
+	Person
+		.findByIdAndDelete(request.params.id)
+		.then(result => {
+			response.json(result).status(204)
+		})
+		.catch(error => next(error))
 })
 
 const generateId = () => {
-  const maxId = Persons.length > 0
-    ? Math.max(...Persons.map(n => n.id))
-    : 0
-  return maxId + 1
+	const maxId = Persons.length > 0
+		? Math.max(...Persons.map(n => n.id))
+		: 0
+	return maxId + 1
 }
 
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body
+	const body = request.body
 
-  if (!body.name) {
-      return response.status(400).json({
-          error: 'name not specified'
-      })
-  }
+	if (!body.name) {
+		return response.status(400).json({
+			error: 'name not specified'
+		})
+	}
 
-  if (!body.number) {
-      return response.status(400).json({
-          error: 'number not specified'
-      })
-  }
+	if (!body.number) {
+		return response.status(400).json({
+			error: 'number not specified'
+		})
+	}
 
-  const person = new Person({
-      name: body.name,
-      number: body.number,
-  })
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
 
-  person
-      .save().then(savedPerson => {
-          response.json(savedPerson)
-      })
-      .catch(error => next(error))
+	person
+		.save().then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+	const body = request.body
 
-  const person = {
-      name: body.name,
-      number: body.number,
-  }
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
 
-  Person
-      .findByIdAndUpdate(request.params.id, person, { new: true })
-      .then(updatedPerson => {
-          response.json(updatedPerson)
-      })
-      .catch(error => next(error))
+	Person
+		.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
